c4b0fe698a5504a7b550cb8063129b6ce0041b9f
diksha0562 <30476228+diksha0562@users.noreply.github.com>
2018-03-06 22:33:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,16 +24,15 @@ app.use(passport.session());
 
 //We dont commit this, bcoz then anyone can go through database and can make changes
 mongoose.connect(keys.mongoURI);// inside this we pass address of mongoose instance
-app.get('/',(req,res)=>{
-res.send({"bye":'buddy'});
-});
+
 routesConfig(app);
 require('./routes/billingRoutes')(app);
 if(process.env.NODE_ENV === 'production'){
-    //Express will serve production assets.. link main.js or main.css file
-    app.use(express.static('client/build'))
-    //Express will serve up the index.html file if doesn't decognize the route
     const path = require('path');
+    //Express will serve production assets.. link main.js or main.css file
+    app.use(express.static(__dirname+'/client/build'));
+    //Express will serve up the index.html file if doesn't decognize the route
+    
     app.get('*',(req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
